10486d1fa8adb1379e01353e4236737d991493ca
Emily Aguirre <emgal.aguirre@gmail.com>
2020-11-24 17:15:08 -0500

    udemy course function practice

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,13 @@
+const userInput = "15";
+let result = "";
+
+result = userInput + 18;
+
+result = result - 2;
+result = result * 20;
+result = result / 3;
+
+// the user input did not change becuase the user input is a constant and the value is refered to by the result variable.
+
+alert("results = " + result);
+alert("user input = " + userInput);
